a75af72f2fb870769a1e1921304f4439c1decb1c
haminsu1234 <136421996+haminsu1234@users.noreply.github.com>
2023-10-20 15:32:54 +0900

    Update SboardApplicationTests.java

--- src/test/java/kr/co/sboard/SboardApplicationTests.java
+++ src/test/java/kr/co/sboard/SboardApplicationTests.java
@@ -3,7 +3,7 @@ package kr.co.sboard;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class SboardApplicationTests {
 
 	@Test
